51c8dd240da3a531d1d16852ccb49524bc4d4ef3
lamtd <lamtd@fireapps.vn>
2019-08-16 17:49:26 +0700

    deploy

---
+++ 3.bundle.9ab7166bf96a97055d6f.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{23:function(t,e,s){"use strict";s.r(e);var i={name:"ProductDetail",props:{}},n=s(5),r=Object(n.a)(i,function(){var t=this.$createElement,e=this._self._c||t;return e("div",[e("h1",[this._v("Product Detail "+this._s(this.$route.params.id))])])},[],!1,null,null,null);e.default=r.exports}}]);
